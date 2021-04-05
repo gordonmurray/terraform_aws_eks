ed939a12f8de16f53d56a99c47b412239fc40e8e
Gordon Murray <gordon@gordonmurray.com>
2021-04-05 18:44:05 +0100

    kube version 1.19

--- eks.tf
+++ eks.tf
@@ -9,14 +9,14 @@ data "aws_eks_cluster_auth" "cluster" {
 module "my-cluster" {
   source          = "terraform-aws-modules/eks/aws"
   cluster_name    = "gordon-terraform-eks"
-  cluster_version = "1.17"
+  cluster_version = "1.19"
   subnets         = ["subnet-0c91cf75e5da4c2a8", "subnet-03fdb36029c4ce2cd"]
   vpc_id          = "vpc-07786849f225d751a"
 
   worker_groups = [
     {
       instance_type      = "m5.large"
-      asg_max_size       = 3
+      asg_max_size       = 2
       spot_price         = "0.0370"
       kubelet_extra_args = "--node-labels=node.kubernetes.io/lifecycle=spot"
     }
