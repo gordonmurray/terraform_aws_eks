5eef081190a36e225222f08ecdcf782b3a32f1e1
Gordon Murray <gordon@gordonmurray.com>
2021-04-05 20:33:33 +0100

    add helmprovider

--- main.tf
+++ main.tf
@@ -12,6 +12,11 @@ terraform {
       source  = "hashicorp/kubernetes"
       version = "2.0.2"
     }
+
+    helm = {
+      source  = "hashicorp/helm"
+      version = "1.3.2"
+    }
   }
 
 }
@@ -26,4 +31,14 @@ provider "kubernetes" {
   host                   = data.aws_eks_cluster.cluster.endpoint
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
   token                  = data.aws_eks_cluster_auth.cluster.token
+}
+
+
+provider "helm" {
+  kubernetes {
+    host                   = data.aws_eks_cluster.cluster.endpoint
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+    token                  = data.aws_eks_cluster_auth.cluster.token
+    load_config_file       = false
+  }
 }
