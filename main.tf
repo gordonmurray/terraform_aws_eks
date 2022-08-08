2db1b4324d531a754603959acd64b278b8d37f0b
Gordon Murray <gordon@gordonmurray.com>
2022-08-08 21:49:03 +0100

    remove helm

--- main.tf
+++ main.tf
@@ -12,11 +12,6 @@ terraform {
       source  = "hashicorp/kubernetes"
       version = "2.12.1"
     }
-
-    helm = {
-      source  = "hashicorp/helm"
-      version = "2.6.0"
-    }
   }
 
 }
@@ -24,21 +19,11 @@ terraform {
 provider "aws" {
   region                   = "eu-west-1"
   shared_credentials_files = ["~/.aws/credentials"]
-  profile                  = "default"
+  profile                  = "gordonmurray"
 }
 
 provider "kubernetes" {
   host                   = data.aws_eks_cluster.cluster.endpoint
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
   token                  = data.aws_eks_cluster_auth.cluster.token
-}
-
-
-provider "helm" {
-  kubernetes {
-    host                   = data.aws_eks_cluster.cluster.endpoint
-    cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
-    token                  = data.aws_eks_cluster_auth.cluster.token
-    load_config_file       = false
-  }
 }
--- readme.md
+++ readme.md
@@ -5,6 +5,7 @@ Before you begin, make sure you have the following installed:
 * Terraform - version 1.1.8 [link](https://www.terraform.io/downloads.html)
 * AWS CLI [link](https://docs.aws.amazon.com/cli/latest/userguide/cli-chap-welcome.html)
 * Kubectl [link](https://kubernetes.io/docs/tasks/tools/)
+* Helm [link](https://helm.sh/docs/intro/install/)
 
 To create the cluster:
 
@@ -16,7 +17,7 @@ terraform apply
 Once terraform has created the cluster:
 
 ```
-aws eks update-kubeconfig --name terraform-eks --profile=default --region=eu-west-1
+aws eks update-kubeconfig --name terraform-eks --profile=gordonmurray --region=eu-west-1
 ```
 
 You should see an output similar to :
